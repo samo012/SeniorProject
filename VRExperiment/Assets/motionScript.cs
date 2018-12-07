f3b173f5c6f6c2dd09505320a31a1ec38b3da5b1
Andrew Samole <samole@live.com>
2018-12-07 18:09:37 -0500

    Removed Avatar

---
+++ VRExperiment/Assets/motionScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motionScript : MonoBehaviour {
+
+	float speed;
+	private float time;
+	public Transform rHand;
+	Vector3 newPos;
+
+	// Use this for initialization
+	void Start () {
+		speed = 1f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		newPos = new Vector3 (transform.position.x, transform.position.y, transform.position.z); 
+
+		if (Input.GetKeyDown (KeyCode.Keypad0)) {
+			Debug.Log ("Mult: " + speed);
+			time = 0;
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			speed += 0.01f;
+			transform.Translate (newPos * Time.deltaTime*speed);
+		}
+
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			transform.position = rHand.position;		
+		}
+	}
+}
